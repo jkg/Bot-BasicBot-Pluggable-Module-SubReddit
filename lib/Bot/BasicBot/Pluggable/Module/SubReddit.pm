aca10212f29af486f81b2dae6ed977500b888bdb
James Green <jkg@earth.li>
2014-08-16 10:36:04 +0100

    bump version, update Changes, ready for release

--- lib/Bot/BasicBot/Pluggable/Module/SubReddit.pm
+++ lib/Bot/BasicBot/Pluggable/Module/SubReddit.pm
@@ -13,11 +13,11 @@ Bot::BasicBot::Pluggable::Module::SubReddit - a simple reddit-related helper plu
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
